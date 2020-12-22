9417e9ee1b3cccaf6ce478ab8fd28f7e69a4283c
Sriram Govindan <sriramgovindan@Srirams-MacBook-Pro.local>
2020-12-22 02:57:13 -0800

    incorporated db

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from os.path import expanduser
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from Paugers_comment_Stripper import CommentStripper
+from database import db
+from flask_pymongo import PyMongo
 
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 cors = CORS(app)
@@ -16,57 +18,25 @@ def index():
 
 @app.route('/videoId/<ID>/<APIKEY>', methods =['POST'])
 def get_comments(ID,APIKEY):
-    #path_to_download_folder = expanduser("~") + '/Downloads'
-    #path_to_download_folder = path_to_download_folder + "/Dataset.json"
     strip = CommentStripper(ID,APIKEY)
     strip.video_data()
     x =strip.top_comment_strip()
     y=strip.remove_unwanted(x)
-    return strip.un_cringe(y)
-    #transfer_file = None
-    #with open(path_to_download_folder, "r") as file:
-        #transfer_file = json.load(file)
-    #temp_string = "/" + ID + "video.json"
-    #destination = expanduser("~") + '/Downloads' + temp_string
-    #with open(destination, "w") as saved_file:
-        #json.dump(transfer_file, saved_file)
+    spread_insert = db(strip.un_cringe(y))
+    spread_insert.add_data()
+    return "Succesful"
 
-    #path_to_download_folder = expanduser("~") + '/Downloads'
-    #path_to_download_folder0 = path_to_download_folder + "/extraced.json"
-    #path_to_download_folder1 = path_to_download_folder + "/Dataset.json"
-
-    #os.remove(path_to_download_folder0)
-    #os.remove(path_to_download_folder1)
-
-    #return("Comments Extracted")
 
 @app.route('/videoIdM/<ID>/<APIKEY>', methods =['GET'])
 def get_comments_manual(ID,APIKEY):
-    #path_to_download_folder = expanduser("~") + '/Downloads'
-    #path_to_download_folder = path_to_download_folder + "/Dataset.json"
     strip = CommentStripper(ID,APIKEY)
     strip.video_data()
     x =strip.top_comment_strip()
     y=strip.remove_unwanted(x)
-    return strip.un_cringe(y)
+    spread_insert = db(strip.un_cringe(y))
+    spread_insert.add_data()
+    return "Succesful"
 
-    #transfer_file = None
-
-    # with open(path_to_download_folder, "r") as file:
-    #     transfer_file = json.load(file)
-    # temp_string = "/" + ID + "video.json"
-    # destination = expanduser("~") + '/Downloads' + temp_string
-    # with open(destination, "w") as saved_file:
-    #     json.dump(transfer_file, saved_file)
-    #
-    # path_to_download_folder = expanduser("~") + '/Downloads'
-    # path_to_download_folder0 = path_to_download_folder + "/extraced.json"
-    # path_to_download_folder1 = path_to_download_folder + "/Dataset.json"
-    #
-    # os.remove(path_to_download_folder0)
-    # os.remove(path_to_download_folder1)
-    #
-    # return("Comments Extracted")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
---
+++ database.py
@@ -0,0 +1,25 @@
+import json
+import pymongo
+import ssl
+
+class db:
+
+    def __init__(self, json_string):
+        self.comment_data = json.loads(json_string)
+        channel_id = self.comment_data[0]['videoChannelID']
+        database_name = channel_id
+        CONNECTION_STRING = "mongodb+srv://gsriram:Billion$100@comments.egtqy.mongodb.net/?retryWrites=true&w=majority"
+        client = pymongo.MongoClient(CONNECTION_STRING, ssl_cert_reqs=ssl.CERT_NONE)
+        self.base = client[database_name]
+
+    def add_data(self):
+        collection_name = self.comment_data[0]['videoId']
+        self.base[collection_name].drop()
+        collection = self.base[collection_name]
+
+        for i in self.comment_data:
+            i['_id'] = i.pop('commentId')
+            i.pop('videoId')
+            i.pop('videoChannelID')
+
+            collection.insert_one(i)
